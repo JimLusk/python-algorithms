c820e97bad802686594a3d892826c5d40adbec1e
Jim Lusk <jblusk@gmail.com>
2020-09-29 14:35:35 -0700

    Design Plan-Any 2 numbers in set equal to given #.

---
+++ any-two-numbers-in-set-equal-given.py
@@ -0,0 +1,14 @@
+""" 
+Algorithm Objective
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+Bonus: Can you do this in one pass? 
+"""
+
+# Create a variable to hold set of numbers to traverse
+
+# Iterate over set adding 1s number in set to all that follow in set, if it equals given return true.
+
+
+
+
