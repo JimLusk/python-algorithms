40aaeaa52017c45861804bb8536885a83442e7f1
Jim Lusk <jblusk@gmail.com>
2020-09-29 16:06:25 -0700

    Added objective algorithm implementation.

--- any-two-numbers-in-set-equal-given.py
+++ any-two-numbers-in-set-equal-given.py
@@ -5,10 +5,21 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 Bonus: Can you do this in one pass? 
 """
 
-# Create a variable to hold set of numbers to traverse
+# 1. Create a variable to hold set of numbers to traverse and iterator count variables to use for nested count variables.
+# 2. Iterate over set adding 1s number in set to all that follow in set, if it equals given return true.
+def check_set_for_desired_sum (sumTargetAnyTwo):
+    numberList = [10, 15, 3, 7]
+    length= len(numberList)
+    iter1 = 0
+    iter2 = 0
+    for number in numberList:
+        iter2 = iter1 + 1
+        while iter2 < length:
+            targetValue = number + numberList[iter2]
+            if targetValue == sumTargetAnyTwo:
+                return True
+            iter2 = iter2 + 1
+        iter1 = iter1 + 1
+    return False
 
-# Iterate over set adding 1s number in set to all that follow in set, if it equals given return true.
-
-
-
-
+print (check_set_for_desired_sum(57))
